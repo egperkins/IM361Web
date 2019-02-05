8252d002baf58ded78a9486730dc524ac92d9b9e
Elizabeth <egperkins@mail.bradley.edu>
2019-02-04 18:34:57 -0600

    Print Do Something Cool
    "Print Do Something Cool"

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Henlo world");
+
+console.log("Do something cool!")
+
+
+// to make it work in terminal call [node index.js]
+// can steal from the wold wide web, just link it like crazy
+
+// to commit
