76ad125e024bd5dc8b2db104ff290aabc1eebb2c
Elizabeth <egperkins@mail.bradley.edu>
2019-02-16 15:42:45 -0600

    Test 2
    To see if the web updates

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import Vue from 'vue/dist/vue.js'
 var app = new Vue({
   el: '#app',
   data: {
-    //where are work is
-    Child1: 'Billy' ,
-    counter1: 0 ,
 
-    Child2: 'Annabelle' ,
-    counter2: 0 ,
 
   }
 });
