8d80fe0f520e7447f1b04a92d52df302a3763d41
Elizabeth <egperkins@mail.bradley.edu>
2019-02-17 19:38:59 -0600

    Session3
    I am about to give up, jk Just dont know what I am doing

--- dist/main.js
+++ dist/main.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nCssSyntaxError\\n\\n(33:1) Unknown word\\n\\n \\u001b[90m 31 | \\u001b[39m\\n \\u001b[90m 32 | \\u001b[39m\\n\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 33 | \\u001b[39m</style>\\n \\u001b[90m    | \\u001b[39m\\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\n \\u001b[90m 34 | \\u001b[39m\\n\");\n\n//# sourceURL=webpack:///./src/style.css?./node_modules/css-loader/dist/cjs.js");
+eval("throw new Error(\"Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nCssSyntaxError\\n\\n(22:1) Unknown word\\n\\n \\u001b[90m 20 | \\u001b[39m\\n \\u001b[90m 21 | \\u001b[39m\\n\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 22 | \\u001b[39m</style>\\n \\u001b[90m    | \\u001b[39m\\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\n \\u001b[90m 23 | \\u001b[39m\\n\");\n\n//# sourceURL=webpack:///./src/style.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
