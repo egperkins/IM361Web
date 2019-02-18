59258ebe075f4b95ee3068215edf3d6fd902b4fa
Elizabeth <egperkins@mail.bradley.edu>
2019-02-17 20:30:20 -0600

    3.2

--- dist/main.js
+++ dist/main.js
@@ -93,7 +93,11 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+<<<<<<< HEAD
 eval("throw new Error(\"Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nCssSyntaxError\\n\\n(18:1) Unknown word\\n\\n \\u001b[90m 16 | \\u001b[39m\\u001b[33m}\\u001b[39m\\n \\u001b[90m 17 | \\u001b[39m\\n\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 18 | \\u001b[39m</style>\\n \\u001b[90m    | \\u001b[39m\\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\n \\u001b[90m 19 | \\u001b[39m\\n\");\n\n//# sourceURL=webpack:///./src/style.css?./node_modules/css-loader/dist/cjs.js");
+=======
+eval("throw new Error(\"Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nCssSyntaxError\\n\\n(33:1) Unknown word\\n\\n \\u001b[90m 31 | \\u001b[39m\\n \\u001b[90m 32 | \\u001b[39m\\n\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 33 | \\u001b[39m</style>\\n \\u001b[90m    | \\u001b[39m\\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\n \\u001b[90m 34 | \\u001b[39m\\n\");\n\n//# sourceURL=webpack:///./src/style.css?./node_modules/css-loader/dist/cjs.js");
+>>>>>>> parent of 8d80fe0... Session3
 
 /***/ }),
 
