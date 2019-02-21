dc6aec38f5503c55dcc9787f50e2b9659b32403e
Elizabeth <egperkins@mail.bradley.edu>
2019-02-21 10:42:11 -0600

    Fun Stuff (Cards)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue/dist/vue.js'
 import "./style.css";
 
+
+
+
+
 var app = new Vue({
+
+
+
+
   el: '#app',
   data: {
-      pageName:"home"
-
+      pageName:"home",
+      EmmaCount: 0,
+      JohnCount: 0,
+      SammieCount: 0,
   }
 });
 
